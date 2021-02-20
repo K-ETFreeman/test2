95a1ba437540dcafbc0e8cb6d60271d0b13911e6
Kirill Riman <Riman-work@yandex.ru>
2021-02-20 17:16:36 +0300

    init

--- js/marquiz.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  Marquiz.init({ 
-    id: '5ec25afc0234e30044f67d6d', 
-    autoOpen: false, 
-    autoOpenFreq: 'once', 
-    openOnExit: true 
-  });
-});
